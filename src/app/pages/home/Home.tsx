14847ec6ba0658377ea2565ed22e3c3d25e2fed9
Erick Pomie <erick@aocmultimedia.com>
2022-06-15 01:27:53 -0500

    Adds AOS & CountUp

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import './Home.scss'
 import MASK from '../../../assets/images/MASK.png'
 import ME from '../../../assets/images/ME-MASK.png'
 import WAVE from '../../../assets/images/WAVE_LAYER.png'
+import CountUp from "react-countup";
 
 const Home = () => {
     return (
@@ -12,6 +13,14 @@ const Home = () => {
                 <img className={'image-personal'} src={ME} alt={'mask'}/>
                 <img className={'wave-layer'} src={WAVE} alt={'wave'} />
             </div>
+            <div className={'d-flex direction-column'}>
+                <div className={'heading-one padding-left-30 animate__animated animate__fadeIn animate__delay-1s'}>
+                    Erick Pomié<br/>
+                    FrontEnd Developer <br/>
+                    Based in Ciudad de México
+                </div>
+            </div>
+            <CountUp end={100} duration={10}/>
         </div>
     )
 }
