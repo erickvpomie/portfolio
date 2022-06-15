d1fecc5c8c0e4e6b4b8324c5338634b671c2be10
Erick Pomie <erick@aocmultimedia.com>
2022-06-15 00:44:35 -0500

    Adds header component and home main image

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,9 +1,17 @@
 import 'animate.css'
 import './Home.scss'
+import MASK from '../../../assets/images/MASK.png'
+import ME from '../../../assets/images/ME-MASK.png'
+import WAVE from '../../../assets/images/WAVE_LAYER.png'
 
 const Home = () => {
     return (
         <div className='home-container d-flex'>
+            <div className={'image-container'}>
+                <img className={'image-mask'} src={MASK} alt={'mask'}/>
+                <img className={'image-personal'} src={ME} alt={'mask'}/>
+                <img className={'wave-layer'} src={WAVE} alt={'wave'} />
+            </div>
         </div>
     )
 }
--- src/app/theme/components/Header/Header.tsx
+++ src/app/theme/components/Header/Header.tsx
@@ -1,12 +1,20 @@
 import './Header.scss'
 import favicon from '../../../../favicon.svg'
+import { TbMenu } from 'react-icons/all'
+
 const Header = () => {
     return(
         <div className='header-container'>
-            <div className="logo-container">
-                <img className='logo' src={favicon} alt={''} />
-                <div className={'logo-text'}>ERICK POMIÉ</div>
+            <div className={'d-flex align-items-center padding-left-40'}>
+                <div className="logo-container">
+                    <img className='logo' src={favicon} alt={''} />
+                    <div className={'logo-text'}>POMIÉ</div>
+                </div>
+                <div className={'header-item padding-left-40 active'}>HOME</div>
+                <div className={'header-item'}>PORTFOLIO</div>
+                <div className={'header-item'}>ABOUT</div>
             </div>
+            <TbMenu className={'menu-item padding-right-40'} size={25}/>
         </div>
     )
 }
